2c8f04adf4bb2f8b240f9e90922ab92dbf4dd0d0
Odlopez <Odlopez@inbox.ru>
2018-06-21 07:09:55 +0300

    Создает верстию решения тестового задания

---
+++ gulpfile.js
@@ -0,0 +1,113 @@
+"use strict";
+
+var gulp = require("gulp"); //сам gulp
+var less = require("gulp-less"); //конвертор из less в css
+var plumber = require("gulp-plumber"); //выводит в консоль сообщения об ошибке и не дает npm-ке прекратить работу из-за ошибки
+var postcss = require("gulp-postcss"); //нужен для автопрефиксера
+var autoprefixer = require("autoprefixer"); //сам автопрефиксер
+var server = require("browser-sync").create(); //создает виртуальный сервер, перегружает браузер
+var run = require('run-sequence'); //нужен для запуска пачки тасков последовательно.
+var rename = require("gulp-rename"); //переименовывает файлы
+var del = require('del'); //удаляет файлы и папки
+var csso = require('gulp-csso'); //минимизирует css
+var htmlmin = require('gulp-htmlmin'); //минизирует html
+var uglify = require('gulp-uglify'); //минизирует js
+var pump = require('pump'); //нужен для работы gulp-uglify
+
+gulp.task("style", function() {
+  gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("serve", function() {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/less/**/*.less", ["style"]);
+  gulp.watch("source/*.html", ["html"]).on("change", server.reload);
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+      "source/fonts/**/*.{woff,woff2,ttf}",
+      "source/img/**",
+      "source/js/**",
+      "source/video/**",
+      "source/*.html"
+    ], {
+      base: "source" //эта настройка говорит gulp'у от какой папки отталкиваться, то есть не только скопировать изображения в папке img, но и саму папку тоже.
+    })
+  .pipe(gulp.dest("build"));
+})
+
+gulp.task("html", function (done) {
+  run(
+    "html-del",
+    "html-copy",
+    done
+  );
+});
+
+gulp.task("html-copy", function () {
+  return gulp.src([
+    "source/*.html"
+  ], {
+    base: "source"
+  })
+.pipe(gulp.dest("build"));
+});
+
+gulp.task("html-del", function () {
+  return del("build/*.html");
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task('minify', function () {
+  return gulp.src('build/css/style.css')
+      .pipe(csso())
+      .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('htmlmin', function() {
+  return gulp.src("build/*.html")
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("compress", function (cb) {
+  pump([
+      gulp.src("build/js/*.js"),
+      uglify(),
+      gulp.dest("build/js")
+    ],
+    cb
+  );
+});
+
+gulp.task("build", function (done) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "htmlmin",
+    "compress",
+    done
+  );
+});
---
+++ source/js/script.js
@@ -0,0 +1,97 @@
+'use strict';
+
+var container = document.querySelector('.container');
+var video = document.querySelector('.video');
+var button = document.querySelector('.main__button');
+var mainTitle = document.querySelector('.main__title');
+var svg = document.querySelector('.main__svg');
+var firstCircle = document.querySelector('.main__first-circle');
+var secondCircle = document.querySelector('.main__second-circle');
+var thirdCircle = document.querySelector('.main__third-circle');
+var slider = document.querySelector('.main__slider');
+var pageTwoTitle = document.querySelector('.page-two__title');
+var pageTwoButtonInner = document.querySelector('.page-two__button-inner');
+var pageTwoText = document.querySelector('.page-two__text');
+var logo = document.querySelector('.header__link');
+var pageButton = document.querySelector('.page-two__button');
+var shadow = document.querySelector('.shadow');
+
+var rewindsVideo = function (sec) {
+  video.currentTime = sec;
+}
+
+var onLogoClick = function () {
+  if (video.paused) {
+    video.play();
+    rewindsVideo(5);
+  }
+
+  logo.removeEventListener('click', onLogoClick);
+}
+
+var onPageButtonClick = function () {
+  mainTitle.classList.remove('js-title-closed');
+  slider.classList.remove('js-slider-closed');
+  button.classList.remove('js-button-closed');
+  svg.classList.remove('js-svg-closed');
+  container.classList.remove('js-container-closed');
+  firstCircle.classList.remove('js-first-circle-moved');
+  secondCircle.classList.remove('js-second-circle-moved');
+  thirdCircle.classList.remove('js-third-circle-moved');
+  pageTwoTitle.classList.remove('js-page-title-rotate');
+  pageTwoText.classList.remove('js-page-text-rotate');
+  pageTwoButtonInner.classList.remove('js-page-button-rotate');
+  shadow.classList.remove('js-shadow-closed');
+
+  setTimeout( function () {
+    mainTitle.style.display = 'block';
+    button.style.display = 'block';
+  }, 510);
+}
+
+var onButtonClick = function () {
+  mainTitle.classList.add('js-title-closed');
+  slider.classList.add('js-slider-closed');
+  button.classList.add('js-button-closed');
+  svg.classList.add('js-svg-closed');
+  container.classList.add('js-container-closed');
+  firstCircle.classList.add('js-first-circle-moved');
+  secondCircle.classList.add('js-second-circle-moved');
+  thirdCircle.classList.add('js-third-circle-moved');
+  pageTwoTitle.classList.add('js-page-title-rotate');
+  pageTwoText.classList.add('js-page-text-rotate');
+  pageTwoButtonInner.classList.add('js-page-button-rotate');
+  shadow.classList.add('js-shadow-closed');
+
+  setTimeout( function () {
+    mainTitle.style.display = 'none';
+    button.style.display = 'none';
+  }, 510);
+}
+
+var onVideoPlay = function () {
+  rewindsVideo(5);
+
+  video.removeEventListener('play', onVideoPlay);
+};
+
+var onThirdCircleAnimationstart = function () {
+  rewindsVideo(28);
+
+  thirdCircle.addEventListener('animationstart', onThirdCircleAnimationstart);
+};
+
+var onDocumentLoaded = function () {
+  if (video.paused) {
+    video.play();
+  }
+
+  document.removeEventListener('DOMContentLoaded', onDocumentLoaded);
+}
+
+pageButton.addEventListener('click', onPageButtonClick);
+thirdCircle.addEventListener('animationstart', onThirdCircleAnimationstart);
+button.addEventListener('click', onButtonClick);
+video.addEventListener('play', onVideoPlay);
+logo.addEventListener('click', onLogoClick);
+document.addEventListener('DOMContentLoaded', onDocumentLoaded);
